a6a5ceb5b5b22a11f9a4fc227e8a8a5f18f25383
Renegmz <renegmz21@gmail.com>
2023-09-05 20:20:43 -0400

    ADD: HACK 4

---
+++ hack_4/hack_4.js
@@ -0,0 +1,22 @@
+/**
+* agregar a los valores del array los tokens:
+* {f:6} y {g:7}
+* dentro del array result 
+* 
+* 
+* output => [{g:7},{a:1},{b:2},{c:3},{d:4},{e:5},{f:6}]
+*/
+let arr = [{a:1},{b:2},{c:3},{d:4},{e:5}];
+let tokenF = {f:6};
+let tokenG = {g:7};
+let result = [];
+
+result.push(tokenG);
+for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i]);
+}
+result.push(tokenF);
+console.log(result);
+
+//export result
+module.exports = result; 
